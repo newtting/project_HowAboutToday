ffddb0523f42ba4a0721f18641f52e0645bb2342
s-oo <d-aoo@naver.com>
2022-10-04 18:32:32 +0900

    🎨 Edit HTML Files
    1. Home
    2. Detail Pages - Notice, Event, About

--- src/main/java/com/phoenix/howabouttoday/member/controller/MemberController.java
+++ src/main/java/com/phoenix/howabouttoday/member/controller/MemberController.java
@@ -2,8 +2,12 @@ package com.phoenix.howabouttoday.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
