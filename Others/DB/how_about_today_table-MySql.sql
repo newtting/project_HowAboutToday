f663a4557b409d2560ca4c43b11b50141b17a3a4
kimyoungwoon <ingngnin@gmail.com>
2022-10-05 18:07:23 +0900

    docs: DB 수정, 테스트 데이터 생성 중

--- Others/DB/how_about_today_table-MySql.sql
+++ Others/DB/how_about_today_table-MySql.sql
@@ -18,24 +18,26 @@ FLUSH PRIVILEGES; -- 모든 권한 적용
 
 --  멤버
 CREATE TABLE T_MEMBER (
-  memberNum int PRIMARY KEY,
+  memberNum int auto_increment,
   email varchar(40) NOT NULL,
   pw varchar(20) NOT NULL,
   nickName varchar(50) NOT NULL,
   memberTel varchar(20) NOT NULL,
-  memberPoint int NOT NULL,
-  memberGrade int NOT NULL --  회원 등급: 일반회원, 판매자, 관리자
-);
+--  memberPoint int NOT NULL, -- 포인트 일단 빼고
+  memberGrade int NOT NULL, --  회원 등급: 일반회원, 판매자, 관리자
+  PRIMARY KEY (`memberNum`)
+)AUTO_INCREMENT=1;
 
 --  쿠폰
 CREATE TABLE T_COUPON (
-  couponNum int PRIMARY KEY,
+  couponNum int auto_increment,
   couponName varchar(50) NOT NULL,
   discountPrice int NOT NULL, --  고정 할인 금액
   couponStartDate datetime NOT NULL,
   couponEndDate datetime NOT NULL,
-  useTerms int NOT NULL  --  사용 조건(ex. 40,000원 이상)
-);
+  useTerms int NOT NULL,  --  사용 조건(ex. 40,000원 이상)
+  PRIMARY KEY (`couponNum`)
+)AUTO_INCREMENT=1;
 
 --  멤버와 쿠폰간의 N:M 관계를 위한 테이블
 CREATE TABLE T_MEMBER_VIEW_COUPON (
@@ -47,34 +49,38 @@ CREATE TABLE T_MEMBER_VIEW_COUPON (
 
 --  지역
 CREATE TABLE T_REGION (
-  regionNum int PRIMARY KEY,
+  regionNum int auto_increment,
   regionName varchar(20) NOT NULL,
-  regionParentNum int NOT NULL
-);
+  regionParentNum int NOT NULL,
+  PRIMARY KEY (`regionNum`)
+)AUTO_INCREMENT=1;
 
 --  숙소 카테고리
 CREATE TABLE T_ACCOM_CATEGORY (
-  accomCategoryNum int PRIMARY KEY,
-  accomCategoryName varchar(50) NOT NULL
-);
+  accomCategoryNum int auto_increment,
+  accomCategoryName varchar(50) NOT NULL,
+  PRIMARY KEY (`accomCategoryNum`)
+)AUTO_INCREMENT=1;
 
 --  숙소
 CREATE TABLE T_ACCOMMODATION (
-  accomNum int PRIMARY KEY,
+  accomNum int auto_increment,
   accomName varchar(50) NOT NULL,
   accomTel varchar(50) NOT NULL,
   accomCategoryNum int NOT NULL,   -- 카테고리 (모텔, 호텔, 펜션)
   regionNum int NOT NULL,       -- 지역
   accomAddress varchar(200) NOT NULL,
   accomRating DECIMAL(2,1) NOT NULL,
+  accomWishListCount int NOT null,
   totalreviewNum int NOT NULL,   -- 숙소의 총 리뷰 수
   latitude decimal(16,14) NOT NULL,	-- 위도
   longitude decimal(17,14) NOT NULL,	-- 경도
   lowPrice int NOT NULL,         -- 숙소의 모든 객실중에서 가장 낮은 가격
   reserveRange int NOT NULL,      -- 예약 가능 범위(예를 들어서 60으로 설정해두면 오늘로부터 +60일까지만 예약이 가능합니다.)
+  PRIMARY KEY (`accomNum`),
   CONSTRAINT FK_9 FOREIGN KEY (regionNum) REFERENCES T_REGION(regionNum)on delete cascade,
   CONSTRAINT FK_25 FOREIGN KEY (accomCategoryNum) REFERENCES T_ACCOM_CATEGORY (accomCategoryNum)on delete cascade
-);
+)AUTO_INCREMENT=1;
 
 --  숙소 이미지
 CREATE TABLE T_ACCOM_IMAGE (
@@ -87,7 +93,7 @@ CREATE TABLE T_ACCOM_IMAGE (
 --  객실
 CREATE TABLE T_ROOM (
   accomNum int,
-  roomNum int PRIMARY KEY,
+  roomNum int auto_increment,
   roomName varchar(50) NOT NULL,
   defaultGuest int NOT NULL,
   maxGuest int NOT NULL,
@@ -100,8 +106,9 @@ CREATE TABLE T_ROOM (
   restStartTime datetime NOT NULL,
   restEndTime datetime NOT NULL,
   roomInfo varchar(1000) NOT NULL,
+  PRIMARY KEY (`roomNum`),
   CONSTRAINT FK_26 FOREIGN KEY (accomNum) REFERENCES T_ACCOMMODATION(accomNum)on delete cascade
-);
+)AUTO_INCREMENT=1;
 -- 원래는 객실 가격을 테이블로 나눴는데 굳이 필요가 없다는 판단이 들어서 객실테이블로 합쳤습니다.
 
 --  객실 정보
@@ -149,9 +156,10 @@ CREATE TABLE T_CART (
 
 --  편의 시설
 CREATE TABLE T_FACILITIES (
-  facilityNum int PRIMARY KEY,
-  facilityName varchar(50) NOT NULL
-);
+  facilityNum int auto_increment,
+  facilityName varchar(50) NOT NULL,
+  PRIMARY KEY (`facilityNum`)
+)AUTO_INCREMENT=1;
 
 --  찜하기
 CREATE TABLE T_WISHLIST (
@@ -172,9 +180,10 @@ CREATE TABLE T_WISHLIST (
 
 -- 예약 상태
 CREATE TABLE T_RESERVATION_STATE (
-  reserveStatus int PRIMARY KEY,
-  orderStatus varchar(20)
-);
+  reserveStatus int auto_increment,
+  orderStatus varchar(20),
+  PRIMARY KEY (`reserveStatus`)
+)AUTO_INCREMENT=1;
 -- 상태는 아마 3개정도? 뿐이겠지만 이렇게 나눠 놓는 게 추후에 상태가 추가 되면 더 구현에 좋을 것 같아서 테이블로 만들었습니다.
 
 --  예약
@@ -182,7 +191,7 @@ CREATE TABLE T_RESERVATION (
   memberNum int,
   accomNum int,
   reserveStatus int,
-  reserveNum int PRIMARY KEY,
+  reserveNum int auto_increment,
   orderNum int NOT NULL,
   orderDate datetime NOT NULL,
   roomName varchar(50) NOT NULL,
@@ -190,15 +199,16 @@ CREATE TABLE T_RESERVATION (
   useEndDate datetime NOT NULL,
   vehicle varchar(10) NOT NULL,
   price int NOT NULL,
+  PRIMARY KEY (`reserveNum`),
   CONSTRAINT FK_16 FOREIGN KEY (memberNum) REFERENCES T_MEMBER(memberNum)on delete cascade,
   CONSTRAINT FK_5 FOREIGN KEY (reserveStatus) REFERENCES T_RESERVATION_STATE(reserveStatus)on delete cascade,
   CONSTRAINT FK_17 FOREIGN KEY (accomNum) REFERENCES T_ACCOMMODATION(accomNum)on delete cascade
-);
+)AUTO_INCREMENT=1;
 
 --  숙소와 편의시설간의 N:M 관계를 위한 테이블
 CREATE TABLE T_ACCOM_VIEW_FACILITIES (
   accomNum int,
-  facilities int,      --@@@
+  facilities int,
   CONSTRAINT FK_21 FOREIGN KEY (accomNum) REFERENCES T_ACCOMMODATION(accomNum)on delete cascade,
   CONSTRAINT FK_22 FOREIGN KEY (facilities) REFERENCES T_FACILITIES(facilityNum)on delete cascade
 );
@@ -209,17 +219,18 @@ CREATE TABLE T_REVIEW (
   accomNum int,
   roomNum int,
   reserveNum int,
-  reviewNum int PRIMARY KEY,
+  reviewNum int auto_increment,
   reviewRating DECIMAL(2,1) NOT NULL,      -- 평점
   reviewCreate datetime NOT NULL,
   reviewContent varchar(1000) NOT NULL,    -- 내용.
+  PRIMARY KEY (`reviewNum`),
   CONSTRAINT FK_4 FOREIGN KEY (reserveNum) REFERENCES T_RESERVATION(reserveNum)on delete cascade,
   CONSTRAINT FK_6 FOREIGN KEY (memberNum) REFERENCES T_MEMBER(memberNum)on delete cascade,
   CONSTRAINT FK_7 FOREIGN KEY (accomNum) REFERENCES T_ACCOMMODATION(accomNum)on delete cascade,
   CONSTRAINT FK_8 FOREIGN KEY (roomNum) REFERENCES T_ROOM(roomNum)on delete cascade
-);
+)AUTO_INCREMENT=1;
 
---  리뷰이미지   
+--  리뷰이미지
 CREATE TABLE T_REVIEW_IMAGE (
   reviewNum int,
   reviewOriginFileName varchar(100) NOT NULL,
@@ -234,34 +245,37 @@ CREATE TABLE T_REPLY (
   roomNum int,
   reserveNum int,
   reviewNum int,
-  replyNum int PRIMARY KEY,
+  replyNum int auto_increment,
   replyCreate datetime NOT NULL,
   replyContent varchar(1000) NOT NULL,
-  CONSTRAINT FK_6 FOREIGN KEY (memberNum) REFERENCES T_MEMBER(memberNum)on delete cascade,
-  CONSTRAINT FK_7 FOREIGN KEY (accomNum) REFERENCES T_ACCOMMODATION(accomNum)on delete cascade,
-  CONSTRAINT FK_8 FOREIGN KEY (roomNum) REFERENCES T_ROOM(roomNum)on delete cascade,
-  CONSTRAINT FK_4 FOREIGN KEY (reserveNum) REFERENCES T_RESERVATION(reserveNum)on delete cascade,
-  CONSTRAINT FK_4 FOREIGN KEY (reviewNum) REFERENCES T_REVIEW(reviewNum)on delete cascade,
-);
+  PRIMARY KEY (`replyNum`),
+  CONSTRAINT FK_36 FOREIGN KEY (memberNum) REFERENCES T_MEMBER(memberNum)on delete cascade,
+  CONSTRAINT FK_37 FOREIGN KEY (accomNum) REFERENCES T_ACCOMMODATION(accomNum)on delete cascade,
+  CONSTRAINT FK_38 FOREIGN KEY (roomNum) REFERENCES T_ROOM(roomNum)on delete cascade,
+  CONSTRAINT FK_44 FOREIGN KEY (reserveNum) REFERENCES T_RESERVATION(reserveNum)on delete cascade,
+  CONSTRAINT FK_34 FOREIGN KEY (reviewNum) REFERENCES T_REVIEW(reviewNum)on delete cascade
+)AUTO_INCREMENT=1;
 
 -- 게시판 카테고리
 CREATE TABLE T_BOARD_CATEGORY(
-	boardCategoryNum int PRIMARY KEY,
+	boardCategoryNum int auto_increment,
 	boardCategoryName varchar(20) NOT NULL,
-	boardParentNum int NOT NULL
-);
+	boardParentNum int NOT NULL,
+	PRIMARY KEY (`boardCategoryNum`)
+)AUTO_INCREMENT=1;
 
 
 -- 게시판
 CREATE TABLE T_BOARD (
-	boardNum int PRIMARY KEY,
+	boardNum int auto_increment,
 	boardCategoryNum int NOT NULL,
 	boardTitle varchar(200) NOT NULL,
 	boardContent varchar(3096) NOT NULL,
 	boardCreate datetime NOT NULL,
 	boardHits int NOT NULL,
+	PRIMARY KEY (`boardNum`),
 CONSTRAINT FK_27 FOREIGN KEY (boardCategoryNum) REFERENCES T_BOARD_CATEGORY(boardCategoryNum) on delete cascade
-);
+)AUTO_INCREMENT=1;
 
 DROP TABLE T_MEMBER;
 DROP TABLE T_COUPON;
@@ -281,6 +295,7 @@ DROP TABLE T_RESERVATION_STATE;
 DROP TABLE T_RESERVATION;
 DROP TABLE T_ACCOM_VIEW_FACILITIES;
 DROP TABLE T_REVIEW;
+DROP TABLE T_REPLY;
 DROP TABLE T_REVIEW_IMAGE;
 DROP TABLE T_BOARD;
 DROP TABLE T_BOARD_CATEGORY;
