069228f9b692390fd8b88b77b6f2b666de364ebf
kyw <ingngnin@gmail.com>
2022-10-06 03:08:49 +0900

    docs: 데이터베이스 board 보드 테이블 제외하고 lowdata 입력

--- Others/DB/insertData/05.t_accom_category_insertData.sql
+++ Others/DB/insertData/05.t_accom_category_insertData.sql
@@ -1,29 +1,13 @@
-CREATE TABLE T_REGION (
-  regionNum int auto_increment,
-  regionName varchar(20) NOT NULL,
-  regionParentNum int NOT NULL,
-  PRIMARY KEY (`regionNum`)
+CREATE TABLE T_ACCOM_CATEGORY (
+  accomCategoryNum int auto_increment,
+  accomCategoryName varchar(50) NOT NULL,
+  PRIMARY KEY (`accomCategoryNum`)
 )AUTO_INCREMENT=1;
 
 
-INSERT INTO `T_REGION` (`regionName`,`regionParentNum`)
+INSERT INTO `t_accom_category` (`accomCategoryName`)
 VALUES
-  ("서울",0),
-  ("경기",0),
-  ("인천",0),
-  ("강원",0),
-  ("제주",0),
-  ("대전",0),
-  ("충북",0),
-  ("충남/세종",0),
-  ("부산",0),
-  ("울산",0),
-  ("경남",0),
-  ("대구",0),
-  ("경북",0),
-  ("광주",0),
-  ("전남",0),
-  ("전주/전북",0);
-
-
-
+  ("호텔"),
+  ("모텔"),
+  ("펜션/풀빌라"),
+  ("게하/한옥");
