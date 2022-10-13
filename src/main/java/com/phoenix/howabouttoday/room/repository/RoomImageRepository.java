01493741dc3fdf20d771b436f46ab58b1464fad0
dayoung <dayoungs031@naver.com>
2022-10-13 23:52:33 +0900

    repository

--- src/main/java/com/phoenix/howabouttoday/room/repository/RoomImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.phoenix.howabouttoday.room.repository;
-
-import com.phoenix.howabouttoday.room.entity.RoomImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
-}
--- src/main/java/com/phoenix/howabouttoday/room/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.phoenix.howabouttoday.room.repository;
-
-import com.phoenix.howabouttoday.room.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-
-}
