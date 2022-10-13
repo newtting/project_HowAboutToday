c83b740480895fbf8d469d374271514194b5c2e3
youngjin3 <dudwlsdl515@naver.com>
2022-10-13 10:42:30 +0900

    엔티티

--- src/main/java/com/phoenix/howabouttoday/board/entity/Reply.java
+++ src/main/java/com/phoenix/howabouttoday/board/entity/Reply.java
@@ -1,4 +1,36 @@
 package com.phoenix.howabouttoday.board.entity;
 
+import com.phoenix.howabouttoday.member.entity.Member;
+import com.phoenix.howabouttoday.payment.Order;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name="T_REPLY")
+
 public class Reply {
+
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long reply_num;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_num")
+    private Member member;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_num")
+    private Review review;
+
+
+
+
 }
