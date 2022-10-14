29aecb10990d9f19e26d4d2d82ee7d0952e3bfa9
s-oo <d-aoo@naver.com>
2022-10-14 14:32:00 +0900

    🤝 Merge

--- src/main/java/com/phoenix/howabouttoday/board/entity/BoardImage.java
+++ src/main/java/com/phoenix/howabouttoday/board/entity/BoardImage.java
@@ -1,14 +1,12 @@
 package com.phoenix.howabouttoday.board.entity;
 
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
 @Getter
 @Entity
+@Table(name = "board_image")
 public class BoardImage {
 
     @Id
@@ -16,20 +14,13 @@ public class BoardImage {
     private Long boardImageNum; // 게시글 이미지 번호
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "board_num")
-    private Board board; // 게시글 번호
+    @JoinColumn(name = "board_num", nullable = false)
+    private Board board; // 게시글
 
     @Column(nullable = false)
     private String boardOriginFileName; // 게시글 이미지 기존 파일 이름
 
     @Column(nullable = false)
     private String boardSaveFileName; // 게시글 이미지 저장 파일 이름
-
-    @Builder
-    public BoardImage(Board board, String boardOriginFileName, String boardSaveFileName) {
-        this.board = board;
-        this.boardOriginFileName = boardOriginFileName;
-        this.boardSaveFileName = boardSaveFileName;
-    }
 
 }
