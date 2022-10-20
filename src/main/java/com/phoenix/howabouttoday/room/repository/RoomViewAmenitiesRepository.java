7a688947a135c2d769daa579946d1d2436bf3735
kenhell77 <106804285+hachijenny@users.noreply.github.com>
2022-10-20 18:45:44 +0900

    집커밋

---
+++ src/main/java/com/phoenix/howabouttoday/room/repository/RoomViewAmenitiesRepository.java
@@ -0,0 +1,14 @@
+package com.phoenix.howabouttoday.room.repository;
+
+import com.phoenix.howabouttoday.room.entity.RoomViewAmenities;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface RoomViewAmenitiesRepository extends JpaRepository<RoomViewAmenities,Long> {
+
+    //Amenities List
+    List<RoomViewAmenities> findAllByAmenitiesRoomNum(Long roomNum);
+
+}
