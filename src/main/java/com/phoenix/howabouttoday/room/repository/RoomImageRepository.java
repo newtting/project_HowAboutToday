509694839a07d06f7bc4562e55168002198f4c6e
kenhell77 <106804285+hachijenny@users.noreply.github.com>
2022-10-21 15:37:22 +0900

    aaa

--- src/main/java/com/phoenix/howabouttoday/room/repository/RoomImageRepository.java
+++ src/main/java/com/phoenix/howabouttoday/room/repository/RoomImageRepository.java
@@ -3,5 +3,11 @@ package com.phoenix.howabouttoday.room.repository;
 import com.phoenix.howabouttoday.room.entity.RoomImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomImageRepository extends JpaRepository<RoomImage, Long> {
+
+    //Image List
+    List<RoomImage> findAllByRoom_RoomNum(Long roomNum);
+
 }
