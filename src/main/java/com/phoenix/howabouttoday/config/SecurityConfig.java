4bf9809991a01b4cef052f23c874215f6c25c2dc
ansooeon <tndjs963@gmail.com>
2022-10-24 10:25:38 +0900

    Revert "Merge pull request #135 from ProSleeper/main"
    This reverts commit 00cbaf49caedb04a43481f2d42331b6004d210e2, reversing
    changes made to ef969036cc47c6d49df37e4641bc72a2df5c6c19.

--- src/main/java/com/phoenix/howabouttoday/config/SecurityConfig.java
+++ src/main/java/com/phoenix/howabouttoday/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
@@ -48,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web
-                .ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/template/**", "/error/**");
+                .ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/template/**", "/error/");
     }
 
 //	@Bean
@@ -68,20 +67,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         //인증되지 않은 모든 요청을
         http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
-                .and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-				.antMatchers("/user-dashboard-profile").authenticated()
-				.antMatchers("/user-dashboard-reviews").authenticated()
-				.antMatchers("/user-dashboard-wishlist").authenticated()
-				.antMatchers("/user-dashboard-booking").authenticated()
-                .antMatchers("/user-dashboard-setting").authenticated()
+//				.antMatchers("/user-dashboard-profile").hasRole("MEMBER")
+//				.antMatchers("/user-dashboard-reviews").hasRole("MEMBER")
+//				.antMatchers("/user-dashboard-wishlist").hasRole("MEMBER")
+//				.antMatchers("/user-dashboard-booking").hasRole("MEMBER")
                 .antMatchers("/**" ).permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/home/login")
+                .loginPage("/login")
                 .usernameParameter("email")
                 .passwordParameter("pwd")
                 .loginProcessingUrl("/loginProc")
